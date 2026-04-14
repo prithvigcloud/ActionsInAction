c8d5e794abd035cc2b3fb8c2111302f22de5d016
prithvigcloud <188611472+prithvigcloud@users.noreply.github.com>
2026-04-13 22:31:21 -0500

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -40,3 +40,25 @@ resource "aws_iam_role" "github_actions" {
   }]
   })
   }  
+
+resource "aws_iam_role_policy" "ecr_push" {
+  name = "ecr-push-policy"
+  role = aws_iam_role.github_actions.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = " [
+          "ecr:GetAuthorizationToken",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload",
+          "ecr:PutImage",
+          "ecr:BatchCheckLayerAvailability"
+        ]
+        Resource = "*"
+        }
+]
+}) 
